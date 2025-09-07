c684488286c3a28dd30bbe65f27c83004df959ea
Mokete <ematyabekana@gmail.com>
2025-09-07 11:22:33 +0200

    added initial screens for Cart, Favorites, Home, and Notifications

---
+++ app/(home)/Notifications.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function NotificationsScreen() {
+  return (
+    <View>
+      <Text>Notifications Page</Text>
+    </View>
+  );
+}
