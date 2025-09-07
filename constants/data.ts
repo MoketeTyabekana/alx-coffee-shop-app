f0429d5aab7312c08f66c5fc951f7ad1c4129e7b
Mokete <ematyabekana@gmail.com>
2025-09-07 11:13:40 +0200

    Added coffee data and interface definition
    - Created a new constants file `data.ts` to store an array of coffee objects, each with an id, name, image, and price.
    - Added a new interface file `index.ts` for future interface definitions.

---
+++ constants/data.ts
@@ -0,0 +1,34 @@
+import { Coffee } from "@/interfaces";
+
+export const coffees: Coffee[] = [
+  {
+    id: 1,
+    name: "Espresso",
+    image: require("../assets/images/coffee1.png"),
+    price: 2.5,
+  },
+  {
+    id: 2,
+    name: "Cappuccino",
+    image: require("../assets/images/coffee2.png"),
+    price: 3.0,
+  },
+  {
+    id: 3,
+    name: "Latte",
+    image: require("../assets/images/coffee3.png"),
+    price: 3.5,
+  },
+  {
+    id: 4,
+    name: "Americano",
+    image: require("../assets/images/coffee4.png"),
+    price: 2.8,
+  },
+  {
+    id: 5,
+    name: "Mocha",
+    image: require("../assets/images/coffee5.png"),
+    price: 3.8,
+  },
+];
