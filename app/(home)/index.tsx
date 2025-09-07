c7f0dacea314e76a46dd60a498058eeb66f54ca4
Mokete <ematyabekana@gmail.com>
2025-09-07 13:58:43 +0200

    Added styles for HomeScreen component to enhance layout

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,9 +1,10 @@
+import { styles } from "@/styles/_home";
 import React from "react";
 import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home Page</Text>
     </View>
   );
